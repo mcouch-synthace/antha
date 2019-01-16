9ce665df3d31f63aebecc0f9e0224433293cec08
Haydn King <h.king@synthace.com>
2019-01-16 15:08:26 +0000

    gofmt -s

--- cmd/antha/cmd/run.go
+++ cmd/antha/cmd/run.go
@@ -167,9 +167,9 @@ func (a *runOpt) Run() error {
 	}
 
 	rout, err := execute.Run(ctx, execute.Opt{
-		Target:   t.Target,
-		Workflow: &bundle.Desc,
-		Params:   &bundle.RawParams,
+		Target:                     t.Target,
+		Workflow:                   &bundle.Desc,
+		Params:                     &bundle.RawParams,
 		TransitionalReadLocalFiles: true,
 	})
 	if err != nil {
